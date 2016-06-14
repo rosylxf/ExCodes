badb6d8b6109fa008c0c0b0cdecaa490e84a000b
le2m0vi <le2m0vi@gmail.com>
2016-06-14 18:41:49 +0800

    update

--- Swift基础教程第2版-Codes/使用集合.playground/Contents.swift
+++ Swift基础教程第2版-Codes/使用集合.playground/Contents.swift
@@ -4,4 +4,64 @@ import UIKit
 
 // 使用集合
 
-let candyJar = ["Peppermints", "Gooey Bears", "Happy Ranchers"]
+
+let candyJarA = ["Peppermints", "Gooey Bears"]
+
+let candyJarB = ["Happy Ranchers"]
+
+var candyJarC = candyJarA + candyJarB
+
+candyJarC.insert("pig", atIndex: 0)
+candyJarC[0] = "god"
+
+print(candyJarC)
+
+candyJarC[0...1] = ["a", "b"]
+
+print(candyJarC)
+
+//candyJarC.removeLast()
+candyJarC.removeLast(2)
+
+print(candyJarC)
+
+
+var arrayA = ["abc", 2, "cef", true]
+
+print(arrayA)
+
+
+// 字典
+
+
+var profile = ["name": "lxf"]
+profile["age"] = "18"
+
+print(profile)
+
+var dictA:[String: Int] = ["count": 18]
+
+print(dictA)
+
+
+
+//更新
+
+profile["age"] = "100"
+
+//删除
+
+//profile.removeValueForKey("age")  //或下面
+profile["age"] = nil
+
+print(profile)
+
+
+
+
+
+
+
+
+
+
---
+++ Swift基础教程第2版-Codes/流程控制.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// 3.1 for loop
+
+//3.1.1 计数
+
+for item  in 0...3 {
+    print(item)
+}
+
+print("====")
+
+//3.1.2 包含还是不包含结束数字
+
+for item  in 0..<3 {
+    print(item)
+}
