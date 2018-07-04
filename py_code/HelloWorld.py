59e496570acdd0f8ff226bf9aaefd7001766386f
rosylxf <rosylxf@gmail.com>
2018-07-04 13:50:22 +0800

    update

---
+++ py_code/HelloWorld.py
@@ -0,0 +1,50 @@
+
+# -*- coding: utf-8 -*-
+
+# print('hello')
+#
+# print('The quick brown fox', 'jumps over', 'the lazy dog')
+#
+# name = input('please input your name:')
+# print("hello,", name)
+
+# print(100*24)
+
+# num = 1
+# string = '1'
+# print(num+int(string))
+#
+# print(type(num))
+
+# words = 'words' * 3
+# print(words)
+#
+# word = 'friends'
+# find_the_evil_in_your_friends = \
+#     word[0]+word[2:4]+word[-3:-1]
+# print(find_the_evil_in_your_friends)
+
+# phone_number = '1386-666-0006'
+# hiding_number = phone_number.replace(phone_number[:9],'*' * 9)
+# print(hiding_number)
+
+# print('{} a word she can get what she {} for.'.format('With','came'))
+
+
+file = open('/Users/cbs/Desktop/text.txt', 'w')
+file.write('hello world')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
